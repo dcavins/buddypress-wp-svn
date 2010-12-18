dcavins/buddypress-wp-svn

commit 4b5f894e5561062f42e2d4a1fedac1ca874b1c10

Prevent banned group users posting new topics, and re-joining groups. Fixes #2661 and #2957, props r-a-y for initial patch.
git-svn-id: http://buddypress.svn.wordpress.org/trunk@3537 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-groups/bp-groups-templatetags.php
@@ -31,9 +31,7 @@ class BP_Groups_Template {
 		if ( 'invites' == $type ) {
 			$this->groups = groups_get_invites_for_user( $user_id, $this->pag_num, $this->pag_page, $exclude );
 		} else if ( 'single-group' == $type ) {
-			$group = new stdClass;
-			$group->group_id = BP_Groups_Group::get_id_from_slug($slug);
-			$this->groups    = array( $group );
+			$this->groups = array( $bp->groups->current_group );
 		} else {
 			$this->groups = groups_get_groups( array( 'type' => $type, 'per_page' => $this->pag_num, 'page' => $this->pag_page, 'user_id' => $user_id, 'search_terms' => $search_terms, 'include' => $include, 'exclude' => $exclude, 'populate_extras' => $populate_extras ) );
 		}
@@ -119,9 +117,6 @@ class BP_Groups_Template {
 
 		$this->in_the_loop = true;
 		$this->group = $this->next_group();
-
-		if ( $this->single_group )
-			$this->group = new BP_Groups_Group( $this->group->group_id, true );
 
 		if ( 0 == $this->current_group ) // loop has just started
 			do_action('loop_start');
@@ -1010,21 +1005,43 @@ function bp_group_is_member( $group = false ) {
 	global $bp, $groups_template;
 
 	// Site admins always have access
-	if ( is_super_admin() )
+	if ( $bp->loggedin_user->is_super_admin )
 		return true;
 
-	// Load group if none passed
 	if ( !$group )
 		$group =& $groups_template->group;
 
-	// Check membership
-	if ( null == $group->is_member )
-		$is_member = false;
-	else
-		$is_member = true;
+	return apply_filters( 'bp_group_is_member', !empty( $group->is_member ) );
+}
 
-	// Return
-	return apply_filters( 'bp_group_is_member', $is_member );
+/**
+ * Checks if a user is banned from a group.
+ *
+ * @global object $bp BuddyPress global settings
+ * @global BP_Groups_Template $groups_template Group template loop object
+ * @param object $group Group to check if user is banned from the group
+ * @param int $user_id
+ * @return bool If user is banned from the group or not
+ * @since 1.3
+ */
+function bp_group_is_user_banned( $group = false, $user_id = false ) {
+	global $bp, $groups_template;
+
+	// Site admins always have access
+	if ( $bp->loggedin_user->is_super_admin )
+		return true;
+
+	if ( !$group ) {
+		$group =& $groups_template->group;
+
+		if ( !$user_id )
+			return apply_filters( 'bp_group_is_member_banned', !empty( $group->is_banned ) );
+	}
+
+	if ( !$user_id )
+		$user_id = $bp->loggedin_user->id;
+
+	return apply_filters( 'bp_group_is_user_banned', groups_is_user_banned( $user_id, $group->id ) );
 }
 
 function bp_group_accept_invite_link() {
@@ -1102,9 +1119,19 @@ function bp_has_friends_to_invite( $group = false ) {
 	return true;
 }
 
-function bp_group_new_topic_button() {
-	if ( bp_is_group_forum() && is_user_logged_in() && !bp_is_group_forum_topic() ) {
-		bp_button( array (
+function bp_group_new_topic_button( $group = false ) {
+	echo bp_get_group_new_topic_button();
+}
+	function bp_get_group_new_topic_button( $group = false ) {
+		global $groups_template;
+
+		if ( !$group )
+			$group =& $groups_template->group;
+
+		if ( !is_user_logged_in() || bp_group_is_user_banned() || !bp_is_group_forum() || bp_is_group_forum_topic() )
+			return false;
+
+		$button = bp_button( array (
 			'id'                => 'new_topic',
 			'component'         => 'groups',
 			'must_be_logged_in' => true,
@@ -1116,8 +1143,10 @@ function bp_group_new_topic_button() {
 			'link_text'         => __( 'New Topic', 'buddypress' ),
 			'link_title'        => __( 'New Topic', 'buddypress' ),
 		) );
+
+		// Filter and return the HTML button
+		return bp_get_button( apply_filters( 'bp_get_group_new_topic_button', $button ) );
 	}
-}
 
 function bp_group_join_button( $group = false ) {
 	echo bp_get_group_join_button();
@@ -1128,8 +1157,7 @@ function bp_group_join_button( $group = false ) {
 		if ( !$group )
 			$group =& $groups_template->group;
 
-		// If they're not logged in or are banned from the group, no join button.
-		if ( !is_user_logged_in() || isset( $group->is_banned ) )
+		if ( !is_user_logged_in() || bp_group_is_user_banned() )
 			return false;
 
 		// Group creation was not completed or status is unknown

bp-groups.php
@@ -51,7 +51,8 @@ function groups_setup_nav() {
 
 		/* This is a single group page. */
 		$bp->is_single_item = true;
-		$bp->groups->current_group = new BP_Groups_Group( $group_id );
+		$bp->groups->current_group = groups_get_groups( array( 'type' => 'active', 'include' => $group_id ) );
+		$bp->groups->current_group = $bp->groups->current_group['groups'][0];
 
 		/* Using "item" not "group" for generic support in other components. */
 		if ( is_super_admin() )
@@ -291,10 +292,14 @@ function groups_screen_group_forum() {
 		$topic_id = bp_forums_get_topic_id_from_slug( $topic_slug );
 		$forum_id = groups_get_groupmeta( $bp->groups->current_group->id, 'forum_id' );
 
+		$user_is_banned = false;
+		if ( !is_super_admin() && groups_is_user_banned( $bp->loggedin_user->id, $bp->groups->current_group->id ) )
+			$user_is_banned = true;
+
 		if ( $topic_slug && $topic_id ) {
 
 			/* Posting a reply */
-			if ( !isset( $bp->action_variables[2] ) && isset( $_POST['submit_reply'] ) ) {
+			if ( !$user_is_banned && !isset( $bp->action_variables[2] ) && isset( $_POST['submit_reply'] ) ) {
 				/* Check the nonce */
 				check_admin_referer( 'bp_forums_new_reply' );
 
@@ -372,7 +377,7 @@ function groups_screen_group_forum() {
 			}
 
 			/* Delete a topic */
-			else if ( isset( $bp->action_variables[2] ) && 'delete' == $bp->action_variables[2] && empty( $bp->action_variables[3] ) ) {
+			else if ( !$user_is_banned && isset( $bp->action_variables[2] ) && 'delete' == $bp->action_variables[2] && empty( $bp->action_variables[3] ) ) {
 				/* Fetch the topic */
 				$topic = bp_forums_get_topic_details( $topic_id );
 
@@ -386,14 +391,13 @@ function groups_screen_group_forum() {
 				if ( !groups_delete_group_forum_topic( $topic_id ) )
 					bp_core_add_message( __( 'There was an error deleting the topic', 'buddypress'), 'error' );
 				else
-					bp_core_add_message( __( 'The topic was deleted successfully', 'buddypress') );
 
 				do_action( 'groups_delete_forum_topic', $topic_id );
 				bp_core_redirect( wp_get_referer() );
 			}
 
 			/* Editing a topic */
-			else if ( isset( $bp->action_variables[2] ) && 'edit' == $bp->action_variables[2] && empty( $bp->action_variables[3] ) ) {
+			else if ( !$user_is_banned && isset( $bp->action_variables[2] ) && 'edit' == $bp->action_variables[2] && empty( $bp->action_variables[3] ) ) {
 				/* Fetch the topic */
 				$topic = bp_forums_get_topic_details( $topic_id );
 
@@ -418,7 +422,7 @@ function groups_screen_group_forum() {
 			}
 
 			/* Delete a post */
-			else if ( isset( $bp->action_variables[2] ) && 'delete' == $bp->action_variables[2] && isset( $bp->action_variables[4] ) && $post_id = $bp->action_variables[4] ) {
+			else if ( !$user_is_banned && isset( $bp->action_variables[2] ) && 'delete' == $bp->action_variables[2] && isset( $bp->action_variables[4] ) && $post_id = $bp->action_variables[4] ) {
 				/* Fetch the post */
 				$post = bp_forums_get_post( $post_id );
 
@@ -439,7 +443,7 @@ function groups_screen_group_forum() {
 			}
 
 			/* Editing a post */
-			else if ( isset( $bp->action_variables[2] ) && 'edit' == $bp->action_variables[2] && isset( $bp->action_variables[4] ) && $post_id = $bp->action_variables[4] ) {
+			else if ( !$user_is_banned && isset( $bp->action_variables[2] ) && 'edit' == $bp->action_variables[2] && isset( $bp->action_variables[4] ) && $post_id = $bp->action_variables[4] ) {
 				/* Fetch the post */
 				$post = bp_forums_get_post( $bp->action_variables[4] );
 
@@ -468,19 +472,25 @@ function groups_screen_group_forum() {
 
 			/* Standard topic display */
 			else {
+				if ( $user_is_banned )
+					bp_core_add_message( __( "You have been banned from this group.", 'buddypress' ) );
+
 				bp_core_load_template( apply_filters( 'groups_template_group_forum_topic', 'groups/single/home' ) );
 			}
 
 		} else {
-
 			/* Posting a topic */
 			if ( isset( $_POST['submit_topic'] ) && function_exists( 'bp_forums_new_topic') ) {
 				/* Check the nonce */
 				check_admin_referer( 'bp_forums_new_topic' );
 
-				/* Auto join this user if they are not yet a member of this group */
-				if ( $bp->groups->auto_join && !is_super_admin() && 'public' == $bp->groups->current_group->status && !groups_is_user_member( $bp->loggedin_user->id, $bp->groups->current_group->id ) )
+				if ( $user_is_banned ) {
+				 	$error_message = __( "You have been banned from this group.", 'buddypress' );
+
+				} elseif ( $bp->groups->auto_join && !is_super_admin() && 'public' == $bp->groups->current_group->status && !groups_is_user_member( $bp->loggedin_user->id, $bp->groups->current_group->id ) ) {
+					// Auto join this user if they are not yet a member of this group
 					groups_join_group( $bp->groups->current_group->id, $bp->loggedin_user->id );
+				}
 
 				if ( empty( $_POST['topic_title'] ) )
 					$error_message = __( 'Please provide a title for your forum topic.', 'buddypress' );
@@ -499,9 +509,8 @@ function groups_screen_group_forum() {
 						$redirect = bp_get_group_permalink( $bp->groups->current_group ) . 'forum/topic/' . $topic->topic_slug . '/';
 					}
 				}
-				
+
 				bp_core_redirect( $redirect );
-				
 			}
 
 			do_action( 'groups_screen_group_forum', $topic_id, $forum_id );

bp-themes/bp-default/groups/single/forum.php
@@ -18,7 +18,7 @@
 
 <?php if ( !bp_is_group_forum_topic_edit() && !bp_is_group_forum_topic() ) : ?>
 
-	<?php if ( ( is_user_logged_in() && 'public' == bp_get_group_status() ) || bp_group_is_member() ) : ?>
+	<?php if ( !bp_group_is_user_banned() && ( ( is_user_logged_in() && 'public' == bp_get_group_status() ) || bp_group_is_member() ) ) : ?>
 
 		<form action="" method="post" id="forum-topic-form" class="standard-form">
 			<div id="new-topic-post">

bp-themes/bp-default/groups/single/forum/topic.php
@@ -78,7 +78,7 @@
 
 			<?php if ( bp_get_the_topic_is_last_page() ) : ?>
 
-				<?php if ( bp_get_the_topic_is_topic_open() ) : ?>
+				<?php if ( bp_get_the_topic_is_topic_open() && !bp_group_is_user_banned() ) : ?>
 
 					<div id="post-topic-reply">
 						<p id="post-reply"></p>
@@ -102,7 +102,7 @@
 						<?php wp_nonce_field( 'bp_forums_new_reply' ) ?>
 					</div>
 
-				<?php else : ?>
+				<?php elseif ( !bp_group_is_user_banned() ) : ?>
 
 					<div id="message" class="info">
 						<p><?php _e( 'This topic is closed, replies are no longer accepted.', 'buddypress' ) ?></p>

commit 070ce8e14e19e6fec68605e1efc58387b379323f

Don't ban super admins! Fixes mistake introduced in r3537, see #2661. Props r-a-y
git-svn-id: http://buddypress.svn.wordpress.org/trunk@3538 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-groups/bp-groups-templatetags.php
@@ -1029,7 +1029,7 @@ function bp_group_is_user_banned( $group = false, $user_id = false ) {
 
 	// Site admins always have access
 	if ( $bp->loggedin_user->is_super_admin )
-		return true;
+		return false;
 
 	if ( !$group ) {
 		$group =& $groups_template->group;