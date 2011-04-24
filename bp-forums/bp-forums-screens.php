dcavins/buddypress-wp-svn

commit 405f3234ada7d1cfd4f7605e2e662ab804100f79

Don't show forum directory template on user profile's forums page
git-svn-id: http://buddypress.svn.wordpress.org/trunk@4283 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-forums/bp-forums-screens.php
@@ -3,7 +3,7 @@
 function bp_forums_directory_forums_setup() {
 	global $bp;
 
-	if ( bp_is_forums_component() ) {
+	if ( bp_is_forums_component() && !bp_current_action() && !bp_current_item() ) {
 		if ( bp_forum_directory_is_disabled() )
 			return false;
 
@@ -66,7 +66,7 @@ function bp_forums_directory_forums_setup() {
 		bp_core_load_template( apply_filters( 'bp_forums_template_directory_forums_setup', 'forums/index' ) );
 	}
 }
-add_action( 'bp_screens', 'bp_forums_directory_forums_setup' );
+add_action( 'bp_screens', 'bp_forums_directory_forums_setup', 2 );
 
 function bp_member_forums_screen_topics() {
 	global $bp;
@@ -115,5 +115,4 @@ function bp_forums_screen_single_topic() {
 	bp_core_load_template( apply_filters( 'bp_forums_screen_single_topic', 'forums/single/topic' ) );
 }
 add_action( 'bp_screens', 'bp_forums_screen_single_topic' );
-
 ?>

commit 0599407b7ea152c84825b05d1705e0c2333f09f0

Fix forum tag URL (broken in r4283). Props boonebgorges
git-svn-id: http://buddypress.svn.wordpress.org/trunk@4284 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-forums/bp-forums-screens.php
@@ -3,7 +3,7 @@
 function bp_forums_directory_forums_setup() {
 	global $bp;
 
-	if ( bp_is_forums_component() && !bp_current_action() && !bp_current_item() ) {
+	if ( bp_is_forums_component() && ( !bp_current_action() || 'tag' == bp_current_action() ) && !bp_current_item() ) {
 		if ( bp_forum_directory_is_disabled() )
 			return false;
 