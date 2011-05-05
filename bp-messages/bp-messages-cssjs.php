dcavins/buddypress-wp-svn

commit 664363c416c4a7f186e4955898078ce2e403b0b6

Rename new 'bp-users' component to 'bp-members' for consistency through-out project. Core, Messages, and Friends Components now use the BP_Component class. Split Friends and Messages components into smaller files. Change references to 'profile' to 'xprofile' through-out project for consistency. Introduce 'bp_actions' and 'bp_screens' standard hooks to replace the usage of 'wp' through-out project. Move component loader sequence into bp-core-bootstrap.php, Move old root_component action into 1.3 deprecated file.
git-svn-id: http://buddypress.svn.wordpress.org/trunk@3917 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-messages/bp-messages-cssjs.php
@@ -4,7 +4,7 @@ function messages_add_autocomplete_js() {
 	global $bp;
 
 	// Include the autocomplete JS for composing a message.
-	if ( $bp->current_component == $bp->messages->slug && 'compose' == $bp->current_action ) {
+	if ( bp_is_messages_component() && bp_is_current_action( 'compose' ) ) {
 		add_action( 'wp_head', 'messages_autocomplete_init_jsblock' );
 
 		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
@@ -21,12 +21,12 @@ function messages_add_autocomplete_js() {
 		}
 	}
 }
-add_action( 'wp', 'messages_add_autocomplete_js', 2 );
+add_action( 'bp_actions', 'messages_add_autocomplete_js' );
 
 function messages_add_autocomplete_css() {
 	global $bp;
 
-	if ( $bp->current_component == $bp->messages->slug && 'compose' == $bp->current_action ) {
+	if ( bp_is_messages_component() && bp_is_current_item( 'compose' ) ) {
 		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG )
 			wp_enqueue_style( 'bp-messages-autocomplete', BP_PLUGIN_URL . '/bp-messages/css/autocomplete/jquery.autocompletefb.dev.css', array(), BP_VERSION );
 		else

commit 55e49024867a0ff0700edcb84525f177434552a5

Fixes typo in bp-messages-cssjs.php (introduced in r3917) that kept autocomplete styles from loading. Fixes #3170
git-svn-id: http://buddypress.svn.wordpress.org/trunk@4332 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-messages/bp-messages-cssjs.php
@@ -26,7 +26,7 @@ add_action( 'bp_actions', 'messages_add_autocomplete_js' );
 function messages_add_autocomplete_css() {
 	global $bp;
 
-	if ( bp_is_messages_component() && bp_is_current_item( 'compose' ) ) {
+	if ( bp_is_messages_component() && bp_is_current_action( 'compose' ) ) {
 		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG )
 			wp_enqueue_style( 'bp-messages-autocomplete', BP_PLUGIN_URL . '/bp-messages/css/autocomplete/jquery.autocompletefb.dev.css', array(), BP_VERSION );
 		else