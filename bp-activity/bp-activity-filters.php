dcavins/buddypress-wp-svn

commit b6efabc7568d339d9099f8e17bf62ee75da0ff6a

Add 'BuddyPress' to all 1.5 phpdoc since's.
git-svn-id: http://buddypress.svn.wordpress.org/trunk@5927 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-activity/bp-activity-filters.php
@@ -247,7 +247,7 @@ function bp_activity_at_name_filter( $content, $activity_id = 0 ) {
 /**
  * Catch mentions in saved activity items
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param obj $activity
  *
@@ -297,7 +297,7 @@ function bp_activity_make_nofollow_filter( $text ) {
 /**
  * Truncates long activity entries when viewed in activity streams
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param $text The original activity entry text
  *

bp-activity/bp-activity-functions.php
@@ -15,7 +15,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Checks $bp pages global and looks for directory page
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @global object $bp BuddyPress global settings
  *
@@ -30,7 +30,7 @@ function bp_activity_has_directory() {
 /**
  * Searches through the content of an activity item to locate usernames, designated by an @ sign
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param string $content The content of the activity, usually found in $activity->content
  *
@@ -50,7 +50,7 @@ function bp_activity_find_mentions( $content ) {
 /**
  * Resets a user's unread mentions list and count
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param int $user_id The id of the user whose unread mentions are being reset
  * @uses bp_delete_user_meta()
@@ -63,7 +63,7 @@ function bp_activity_clear_new_mentions( $user_id ) {
 /**
  * Adjusts new mention count for mentioned users when activity items are deleted or created
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param int $activity_id The unique id for the activity item
  * @param string $action Can be 'delete' or 'add'. Defaults to 'add'
@@ -122,7 +122,7 @@ function bp_activity_adjust_mention_count( $activity_id, $action = 'add' ) {
 /**
  * Formats notifications related to activity
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param string $action The type of activity item. Just 'new_at_mention' for now
  * @param int $item_id The activity id
@@ -613,7 +613,7 @@ function bp_activity_update_meta( $activity_id, $meta_key, $meta_value ) {
 /**
  * Completely remove a user's activity data
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param int $user_id
  *
@@ -1519,7 +1519,7 @@ function bp_activity_mark_as_ham( &$activity, $source = 'by_a_person' ) {
  * This does not cover recursive activity comments, as they do not use a real loop.
  * For that, see {@link bp_activity_comment_embed()}.
  *
- * @since 1.5
+ * @since BuddyPress (1.5)
  *
  * @see BP_Embed
  * @see bp_embed_activity_cache()
@@ -1541,7 +1541,7 @@ add_action( 'activity_loop_start', 'bp_activity_embed' );
  * when BP is recursing through activity comments {@link bp_activity_recurse_comments()}.
  * If no cache and link is embeddable, cache it.
  *
- * @since 1.5
+ * @since BuddyPress (1.5)
  *
  * @see BP_Embed
  * @see bp_embed_activity_cache()
@@ -1561,7 +1561,7 @@ add_action( 'bp_before_activity_comment', 'bp_activity_comment_embed' );
 /**
  * When a user clicks on a "Read More" item, make sure embeds are correctly parsed and shown for the expanded content.
  *
- * @since 1.5
+ * @since BuddyPress (1.5)
  *
  * @see BP_Embed
  *
@@ -1588,7 +1588,7 @@ add_action( 'bp_dtheme_get_single_activity_content', 'bp_dtheme_embed_read_more'
  * is rendered to avoid conflict with the 'embed_post_id' filter in
  * {@link bp_activity_embed()} or any other component embeds.
  *
- * @since 1.5
+ * @since BuddyPress (1.5)
  *
  * @see bp_activity_comment_embed()
  *
@@ -1603,7 +1603,7 @@ add_action( 'bp_after_activity_comment', 'bp_activity_comment_embed_after_recurs
  * Wrapper function for {@link bp_activity_get_meta()}.
  * Used during {@link BP_Embed::parse_oembed()} via {@link bp_activity_embed()}.
  *
- * @since 1.5
+ * @since BuddyPress (1.5)
  *
  * @uses bp_activity_get_meta()
  *
@@ -1617,7 +1617,7 @@ function bp_embed_activity_cache( $cache, $id, $cachekey ) {
  * Wrapper function for {@link bp_activity_update_meta()}.
  * Used during {@link BP_Embed::parse_oembed()} via {@link bp_activity_embed()}.
  *
- * @since 1.5
+ * @since BuddyPress (1.5)
  *
  * @uses bp_activity_update_meta()
  */

bp-activity/bp-activity-loader.php
@@ -15,14 +15,14 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Main Activity Class
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  */
 class BP_Activity_Component extends BP_Component {
 
 	/**
 	 * Start the activity component creation process
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 */
 	function __construct() {
 		parent::start(
@@ -35,7 +35,7 @@ class BP_Activity_Component extends BP_Component {
 	/**
 	 * Include files
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 */
 	function includes() {
 		// Files to include
@@ -67,7 +67,7 @@ class BP_Activity_Component extends BP_Component {
 	 * The BP_ACTIVITY_SLUG constant is deprecated, and only used here for
 	 * backwards compatibility.
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $bp BuddyPress global settings
 	 */
@@ -101,7 +101,7 @@ class BP_Activity_Component extends BP_Component {
 	/**
 	 * Setup BuddyBar navigation
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses bp_is_active()
@@ -202,7 +202,7 @@ class BP_Activity_Component extends BP_Component {
 	/**
 	 * Set up the Toolbar
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses is_user_logged_in()
@@ -293,7 +293,7 @@ class BP_Activity_Component extends BP_Component {
 	/**
 	 * Sets up the title for pages and <title>
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses bp_is_activity_component()

bp-activity/bp-activity-screens.php
@@ -13,7 +13,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Activity screen index
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_displayed_user_id()
  * @uses bp_is_activity_component()
@@ -128,7 +128,7 @@ function bp_activity_screen_mentions() {
  * Removes activity notifications from the notification menu when a user clicks on them and
  * is taken to a specific screen.
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @global object $bp BuddyPress global settings
  * @uses bp_core_delete_notifications_by_type()
@@ -145,7 +145,7 @@ add_action( 'bp_activity_screen_mentions',                  'bp_activity_remove_
 /**
  * Reset the logged-in user's new mentions data when he visits his mentions screen
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_is_my_profile()
  * @uses bp_activity_clear_new_mentions()

bp-activity/bp-activity-template.php
@@ -13,7 +13,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Output the activity component slug
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_slug()
  */
@@ -23,7 +23,7 @@ function bp_activity_slug() {
 	/**
 	 * Return the activity component slug
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses apply_filters() To call the 'bp_get_activity_slug' hook
@@ -36,7 +36,7 @@ function bp_activity_slug() {
 /**
  * Output the activity component root slug
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_root_slug()
  */
@@ -46,7 +46,7 @@ function bp_activity_root_slug() {
 	/**
 	 * Return the activity component root slug
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses apply_filters() To call the 'bp_get_activity_root_slug' hook
@@ -59,7 +59,7 @@ function bp_activity_root_slug() {
 /**
  * Output member directory permalink
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_directory_permalink()
  */
@@ -69,7 +69,7 @@ function bp_activity_directory_permalink() {
 	/**
 	 * Return member directory permalink
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @uses traisingslashit()
 	 * @uses bp_get_root_domain()
@@ -487,7 +487,7 @@ function bp_activity_pagination_links() {
 /**
  * Returns true when there are more activity items to be shown than currently appear
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @global object $activities_template {@link BP_Activity_Template}
  * @uses apply_filters() To call the 'bp_activity_has_more_items' hook
@@ -1413,7 +1413,7 @@ function bp_activity_comments( $args = '' ) {
 /**
  * Utility function that returns the comment currently being recursed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @global object $activities_template {@link BP_Activity_Template}
  * @uses apply_filters() To call the 'bp_activity_current_comment' hook
@@ -1432,7 +1432,7 @@ function bp_activity_current_comment() {
 /**
  * Echoes the id of the activity comment currently being displayed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_comment_id()
  */
@@ -1443,7 +1443,7 @@ function bp_activity_comment_id() {
 	/**
 	 * Gets the id of the activity comment currently being displayed
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses apply_filters() To call the 'bp_activity_comment_id' hook
@@ -1461,7 +1461,7 @@ function bp_activity_comment_id() {
 /**
  * Echoes the user_id of the author of the activity comment currently being displayed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_comment_user_id()
  */
@@ -1472,7 +1472,7 @@ function bp_activity_comment_user_id() {
 	/**
 	 * Gets the user_id of the author of the activity comment currently being displayed
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses apply_filters() To call the 'bp_activity_comment_user_id' hook
@@ -1490,7 +1490,7 @@ function bp_activity_comment_user_id() {
 /**
  * Echoes the author link for the activity comment currently being displayed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_comment_user_link()
  */
@@ -1501,7 +1501,7 @@ function bp_activity_comment_user_link() {
 	/**
 	 * Gets the author link for the activity comment currently being displayed
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @uses bp_core_get_user_domain()
 	 * @uses bp_get_activity_comment_user_id()
@@ -1518,7 +1518,7 @@ function bp_activity_comment_user_link() {
 /**
  * Echoes the author name for the activity comment currently being displayed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_comment_name()
  */
@@ -1531,7 +1531,7 @@ function bp_activity_comment_name() {
 	 *
 	 * The use of the bp_acomment_name filter is deprecated. Please use bp_activity_comment_name
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses apply_filters() To call the 'bp_acomment_name' hook
@@ -1553,7 +1553,7 @@ function bp_activity_comment_name() {
 /**
  * Echoes the date_recorded of the activity comment currently being displayed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_comment_date_recorded()
  */
@@ -1564,7 +1564,7 @@ function bp_activity_comment_date_recorded() {
 	/**
 	 * Gets the date_recorded for the activity comment currently being displayed
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses bp_core_time_since()
@@ -1586,7 +1586,7 @@ function bp_activity_comment_date_recorded() {
 /**
  * Echoes the 'delete' URL for the activity comment currently being displayed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_comment_delete_link()
  */
@@ -1597,7 +1597,7 @@ function bp_activity_comment_delete_link() {
 	/**
 	 * Gets the 'delete' URL for the activity comment currently being displayed
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @uses wp_nonce_url()
 	 * @uses bp_get_root_domain()
@@ -1616,7 +1616,7 @@ function bp_activity_comment_delete_link() {
 /**
  * Echoes the content of the activity comment currently being displayed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bp_get_activity_comment_content()
  */
@@ -1631,7 +1631,7 @@ function bp_activity_comment_content() {
 	 * applied to activity items in general. Use bp_activity_comment_content to modify the
 	 * content of activity comments only.
 	 *
-	 * @since 1.5.0
+	 * @since BuddyPress (1.5).0
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses apply_filters() To call the 'bp_get_activity_content' hook
@@ -2170,7 +2170,7 @@ function bp_activity_can_comment() {
 /**
  * Determine if a comment can be made on an activity reply item
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param object $comment Activity comment
  *
@@ -2187,7 +2187,7 @@ function bp_activity_can_comment_reply( $comment ) {
 /**
  * Determine if an favorites are allowed
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses apply_filters() To call the 'bp_activity_can_favorite' hook
  *
@@ -2659,7 +2659,7 @@ function bp_activity_feed_item_description() {
 /**
  * Template tag so we can hook activity feed to <head>
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @uses bloginfo()
  * @uses bp_sitewide_activity_feed_link()

bp-core/bp-core-buddybar.php
@@ -376,7 +376,7 @@ add_action( 'admin_head', 'bp_core_sort_subnav_items' );
  * Determines whether a given nav item has subnav items
  *
  * @package BuddyPress
- * @since 1.5
+ * @since BuddyPress (1.5)
  *
  * @param str $nav_item The id of the top-level nav item whose nav items you're checking
  * @return bool $has_subnav True if the nav item is found and has subnav items; false otherwise
@@ -615,7 +615,7 @@ function bp_adminbar_random_menu() {
  *
  * This is a direct copy of WP's private _get_admin_bar_pref()
  *
- * @since 1.5.0
+ * @since BuddyPress (1.5).0
  *
  * @param string $context Context of this preference check, either 'admin' or 'front'.
  * @param int $user Optional. ID of the user to check, defaults to 0 for current user.

bp-blogs/bp-blogs-functions.php
@@ -5,7 +5,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Checks $bp pages global and looks for directory page
  *
- * @since 1.5
+ * @since BuddyPress (1.5)
  *
  * @global BuddyPress $bp The one true BuddyPress instance
  * @return bool True if set, False if empty

bp-blogs/bp-blogs-loader.php
@@ -17,7 +17,7 @@ class BP_Blogs_Component extends BP_Component {
 	/**
 	 * Start the blogs component creation process
 	 *
-	 * @since 1.5
+	 * @since BuddyPress (1.5)
 	 */
 	function __construct() {
 		parent::start(
@@ -33,7 +33,7 @@ class BP_Blogs_Component extends BP_Component {
 	 * The BP_BLOGS_SLUG constant is deprecated, and only used here for
 	 * backwards compatibility.
 	 *
-	 * @since 1.5
+	 * @since BuddyPress (1.5)
 	 * @global BuddyPress $bp The one true BuddyPress instance
 	 */
 	function setup_globals() {

commit 12db2e9af088da823893a20281738fd389ebfafc

Fix phpdoc since typos introduced in r5927
git-svn-id: http://buddypress.svn.wordpress.org/trunk@5928 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-activity/bp-activity-filters.php
@@ -247,7 +247,7 @@ function bp_activity_at_name_filter( $content, $activity_id = 0 ) {
 /**
  * Catch mentions in saved activity items
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param obj $activity
  *
@@ -297,7 +297,7 @@ function bp_activity_make_nofollow_filter( $text ) {
 /**
  * Truncates long activity entries when viewed in activity streams
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param $text The original activity entry text
  *

bp-activity/bp-activity-functions.php
@@ -15,7 +15,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Checks $bp pages global and looks for directory page
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @global object $bp BuddyPress global settings
  *
@@ -30,7 +30,7 @@ function bp_activity_has_directory() {
 /**
  * Searches through the content of an activity item to locate usernames, designated by an @ sign
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param string $content The content of the activity, usually found in $activity->content
  *
@@ -50,7 +50,7 @@ function bp_activity_find_mentions( $content ) {
 /**
  * Resets a user's unread mentions list and count
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param int $user_id The id of the user whose unread mentions are being reset
  * @uses bp_delete_user_meta()
@@ -63,7 +63,7 @@ function bp_activity_clear_new_mentions( $user_id ) {
 /**
  * Adjusts new mention count for mentioned users when activity items are deleted or created
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param int $activity_id The unique id for the activity item
  * @param string $action Can be 'delete' or 'add'. Defaults to 'add'
@@ -122,7 +122,7 @@ function bp_activity_adjust_mention_count( $activity_id, $action = 'add' ) {
 /**
  * Formats notifications related to activity
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param string $action The type of activity item. Just 'new_at_mention' for now
  * @param int $item_id The activity id
@@ -613,7 +613,7 @@ function bp_activity_update_meta( $activity_id, $meta_key, $meta_value ) {
 /**
  * Completely remove a user's activity data
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param int $user_id
  *

bp-activity/bp-activity-loader.php
@@ -15,14 +15,14 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Main Activity Class
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  */
 class BP_Activity_Component extends BP_Component {
 
 	/**
 	 * Start the activity component creation process
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 */
 	function __construct() {
 		parent::start(
@@ -35,7 +35,7 @@ class BP_Activity_Component extends BP_Component {
 	/**
 	 * Include files
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 */
 	function includes() {
 		// Files to include
@@ -67,7 +67,7 @@ class BP_Activity_Component extends BP_Component {
 	 * The BP_ACTIVITY_SLUG constant is deprecated, and only used here for
 	 * backwards compatibility.
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $bp BuddyPress global settings
 	 */
@@ -101,7 +101,7 @@ class BP_Activity_Component extends BP_Component {
 	/**
 	 * Setup BuddyBar navigation
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses bp_is_active()
@@ -202,7 +202,7 @@ class BP_Activity_Component extends BP_Component {
 	/**
 	 * Set up the Toolbar
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses is_user_logged_in()
@@ -293,7 +293,7 @@ class BP_Activity_Component extends BP_Component {
 	/**
 	 * Sets up the title for pages and <title>
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses bp_is_activity_component()

bp-activity/bp-activity-screens.php
@@ -13,7 +13,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Activity screen index
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_displayed_user_id()
  * @uses bp_is_activity_component()
@@ -128,7 +128,7 @@ function bp_activity_screen_mentions() {
  * Removes activity notifications from the notification menu when a user clicks on them and
  * is taken to a specific screen.
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @global object $bp BuddyPress global settings
  * @uses bp_core_delete_notifications_by_type()
@@ -145,7 +145,7 @@ add_action( 'bp_activity_screen_mentions',                  'bp_activity_remove_
 /**
  * Reset the logged-in user's new mentions data when he visits his mentions screen
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_is_my_profile()
  * @uses bp_activity_clear_new_mentions()

bp-activity/bp-activity-template.php
@@ -13,7 +13,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 /**
  * Output the activity component slug
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_slug()
  */
@@ -23,7 +23,7 @@ function bp_activity_slug() {
 	/**
 	 * Return the activity component slug
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses apply_filters() To call the 'bp_get_activity_slug' hook
@@ -36,7 +36,7 @@ function bp_activity_slug() {
 /**
  * Output the activity component root slug
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_root_slug()
  */
@@ -46,7 +46,7 @@ function bp_activity_root_slug() {
 	/**
 	 * Return the activity component root slug
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $bp BuddyPress global settings
 	 * @uses apply_filters() To call the 'bp_get_activity_root_slug' hook
@@ -59,7 +59,7 @@ function bp_activity_root_slug() {
 /**
  * Output member directory permalink
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_directory_permalink()
  */
@@ -69,7 +69,7 @@ function bp_activity_directory_permalink() {
 	/**
 	 * Return member directory permalink
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @uses traisingslashit()
 	 * @uses bp_get_root_domain()
@@ -487,7 +487,7 @@ function bp_activity_pagination_links() {
 /**
  * Returns true when there are more activity items to be shown than currently appear
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @global object $activities_template {@link BP_Activity_Template}
  * @uses apply_filters() To call the 'bp_activity_has_more_items' hook
@@ -1413,7 +1413,7 @@ function bp_activity_comments( $args = '' ) {
 /**
  * Utility function that returns the comment currently being recursed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @global object $activities_template {@link BP_Activity_Template}
  * @uses apply_filters() To call the 'bp_activity_current_comment' hook
@@ -1432,7 +1432,7 @@ function bp_activity_current_comment() {
 /**
  * Echoes the id of the activity comment currently being displayed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_comment_id()
  */
@@ -1443,7 +1443,7 @@ function bp_activity_comment_id() {
 	/**
 	 * Gets the id of the activity comment currently being displayed
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses apply_filters() To call the 'bp_activity_comment_id' hook
@@ -1461,7 +1461,7 @@ function bp_activity_comment_id() {
 /**
  * Echoes the user_id of the author of the activity comment currently being displayed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_comment_user_id()
  */
@@ -1472,7 +1472,7 @@ function bp_activity_comment_user_id() {
 	/**
 	 * Gets the user_id of the author of the activity comment currently being displayed
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses apply_filters() To call the 'bp_activity_comment_user_id' hook
@@ -1490,7 +1490,7 @@ function bp_activity_comment_user_id() {
 /**
  * Echoes the author link for the activity comment currently being displayed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_comment_user_link()
  */
@@ -1501,7 +1501,7 @@ function bp_activity_comment_user_link() {
 	/**
 	 * Gets the author link for the activity comment currently being displayed
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @uses bp_core_get_user_domain()
 	 * @uses bp_get_activity_comment_user_id()
@@ -1518,7 +1518,7 @@ function bp_activity_comment_user_link() {
 /**
  * Echoes the author name for the activity comment currently being displayed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_comment_name()
  */
@@ -1531,7 +1531,7 @@ function bp_activity_comment_name() {
 	 *
 	 * The use of the bp_acomment_name filter is deprecated. Please use bp_activity_comment_name
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses apply_filters() To call the 'bp_acomment_name' hook
@@ -1553,7 +1553,7 @@ function bp_activity_comment_name() {
 /**
  * Echoes the date_recorded of the activity comment currently being displayed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_comment_date_recorded()
  */
@@ -1564,7 +1564,7 @@ function bp_activity_comment_date_recorded() {
 	/**
 	 * Gets the date_recorded for the activity comment currently being displayed
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses bp_core_time_since()
@@ -1586,7 +1586,7 @@ function bp_activity_comment_date_recorded() {
 /**
  * Echoes the 'delete' URL for the activity comment currently being displayed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_comment_delete_link()
  */
@@ -1597,7 +1597,7 @@ function bp_activity_comment_delete_link() {
 	/**
 	 * Gets the 'delete' URL for the activity comment currently being displayed
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @uses wp_nonce_url()
 	 * @uses bp_get_root_domain()
@@ -1616,7 +1616,7 @@ function bp_activity_comment_delete_link() {
 /**
  * Echoes the content of the activity comment currently being displayed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bp_get_activity_comment_content()
  */
@@ -1631,7 +1631,7 @@ function bp_activity_comment_content() {
 	 * applied to activity items in general. Use bp_activity_comment_content to modify the
 	 * content of activity comments only.
 	 *
-	 * @since BuddyPress (1.5).0
+	 * @since BuddyPress (1.5)
 	 *
 	 * @global object $activities_template {@link BP_Activity_Template}
 	 * @uses apply_filters() To call the 'bp_get_activity_content' hook
@@ -2170,7 +2170,7 @@ function bp_activity_can_comment() {
 /**
  * Determine if a comment can be made on an activity reply item
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param object $comment Activity comment
  *
@@ -2187,7 +2187,7 @@ function bp_activity_can_comment_reply( $comment ) {
 /**
  * Determine if an favorites are allowed
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses apply_filters() To call the 'bp_activity_can_favorite' hook
  *
@@ -2659,7 +2659,7 @@ function bp_activity_feed_item_description() {
 /**
  * Template tag so we can hook activity feed to <head>
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @uses bloginfo()
  * @uses bp_sitewide_activity_feed_link()

bp-core/bp-core-buddybar.php
@@ -615,7 +615,7 @@ function bp_adminbar_random_menu() {
  *
  * This is a direct copy of WP's private _get_admin_bar_pref()
  *
- * @since BuddyPress (1.5).0
+ * @since BuddyPress (1.5)
  *
  * @param string $context Context of this preference check, either 'admin' or 'front'.
  * @param int $user Optional. ID of the user to check, defaults to 0 for current user.