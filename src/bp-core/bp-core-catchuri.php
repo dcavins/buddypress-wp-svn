dcavins/buddypress-wp-svn

commit 515b2ab7c47b456d023b1e2f876108fb086dd693

In `bp_core_load_template()`, reset post globals if we're on a non-directory page.
If we're on a non-directory page, we are faking a WordPress page, so we
must reset the WordPress post globals to avoid notices from appearing.  The
reset is done with the `bp_theme_compat_reset_post()` function and notably
fixes notices when the root profiles feature is toggled.

Commit also removes an unnecessary database query to determine whether
we're on a BuddyPress directory page.  WordPress should have already made
this determination before BuddyPress has reached this point in
`bp_core_load_template()`.

Fixes #5241.

git-svn-id: http://buddypress.svn.wordpress.org/trunk@8821 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-core/bp-core-catchuri.php
@@ -340,30 +340,21 @@ function bp_core_enable_root_profiles() {
  * @return bool|null Returns false on failure.
  */
 function bp_core_load_template( $templates ) {
-	global $post, $bp, $wp_query, $wpdb;
+	global $wp_query;
 
-	// Determine if the root object WP page exists for this request
-	// note: get_page_by_path() breaks non-root pages
-	if ( !empty( $bp->unfiltered_uri_offset ) ) {
-		if ( !$page_exists = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM {$wpdb->posts} WHERE post_name = %s", $bp->unfiltered_uri[$bp->unfiltered_uri_offset] ) ) ) {
-			return false;
-		}
-	}
+	// reset post if not on a directory page
+	// if we're not on a directory page, this means we're faking a page and we
+	// need to reset the post to avoid notices
+	if ( ! bp_is_directory() ) {
+		bp_theme_compat_reset_post( array(
+			'ID'          => 0,
+			'is_404'      => true,
+			'post_status' => 'publish',
+		) );
 
-	// Set the root object as the current wp_query-ied item
-	$object_id = 0;
-	foreach ( (array) $bp->pages as $page ) {
-		if ( $page->name == $bp->unfiltered_uri[$bp->unfiltered_uri_offset] ) {
-			$object_id = $page->id;
-		}
-	}
-
-	// Make the queried/post object an actual valid page
-	if ( !empty( $object_id ) ) {
-		$wp_query->queried_object    = get_post( $object_id );
-		$wp_query->queried_object_id = $object_id;
-		$wp_query->post              = $wp_query->queried_object;
-		$post                        = $wp_query->queried_object;
+		// need to set theme compat to false since the reset post function
+		// automatically sets theme compat to true
+		bp_set_theme_compat_active( false );
 	}
 
 	// Fetch each template and add the php suffix

commit 18f738a930d7fc5a90e9769047970ab3818fe406

Fixes a regression in r8821 in `rbp_core_load_template()` which was preventing the correct parent page being highlighted in WP nav menus.
This change reinstates the page look up and post globals as before r8821. Overall, these changes are still a net benefit as an uncached SQL query has been removed. See #5241 for background.

Fixes #5810

git-svn-id: http://buddypress.svn.wordpress.org/trunk@8932 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-core/bp-core-catchuri.php
@@ -342,20 +342,38 @@ function bp_core_enable_root_profiles() {
 function bp_core_load_template( $templates ) {
 	global $wp_query;
 
-	// reset post if not on a directory page
-	// if we're not on a directory page, this means we're faking a page and we
-	// need to reset the post to avoid notices
-	if ( ! bp_is_directory() ) {
-		bp_theme_compat_reset_post( array(
-			'ID'          => 0,
-			'is_404'      => true,
-			'post_status' => 'publish',
-		) );
-
-		// need to set theme compat to false since the reset post function
-		// automatically sets theme compat to true
-		bp_set_theme_compat_active( false );
+	// check if BP page belongs to, or is a child of, a BP directory page
+	$page_id = false;
+	foreach ( (array) buddypress()->pages as $page ) {
+		if ( $page->name == buddypress()->unfiltered_uri[buddypress()->unfiltered_uri_offset] ) {
+			$page_id = $page->id;
+			break;
+		}
 	}
+
+	// Set up reset post args
+	$reset_post_args = array(
+		'is_404'      => true,
+		'post_status' => 'publish',
+	);
+
+	// BP page exists - fill in the $wp_query->post object
+	//
+	// bp_theme_compat_reset_post() looks at the $wp_query->post object to fill in
+	// the post globals
+	if ( ! empty( $page_id ) ) {
+		$wp_query->post = get_post( $page_id );
+		$reset_post_args['ID'] = $page_id;
+	} else {
+		$reset_post_args['ID'] = 0;
+	}
+
+	// Reset the post
+	bp_theme_compat_reset_post( $reset_post_args );
+
+	// Set theme compat to false since the reset post function automatically sets
+	// theme compat to true
+	bp_set_theme_compat_active( false );
 
 	// Fetch each template and add the php suffix
 	$filtered_templates = array();