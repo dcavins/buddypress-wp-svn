dcavins/buddypress-wp-svn

commit e825786325a0c153727f7a5709418feeb12af31b

Cleaning up and adding file headers for BP Templates Component.
See #6644.


git-svn-id: http://buddypress.svn.wordpress.org/trunk@10181 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-templates/bp-legacy/buddypress/members/single/settings/notifications.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * BuddyPress - Members Settings Notifications
+ *
+ * @package BuddyPress
+ * @subpackage bp-legacy
+ */
+
+?>
 
 /** This action is documented in bp-templates/bp-legacy/buddypress/members/single/settings/profile.php */
 do_action( 'bp_before_member_settings_template' ); ?>

src/bp-templates/bp-legacy/buddypress/activity/activity-loop.php
@@ -2,7 +2,7 @@
 /**
  * BuddyPress - Activity Loop
  *
- * @package    BuddyPress
+ * @package BuddyPress
  * @subpackage bp-legacy
  */
 

src/bp-templates/bp-legacy/buddypress/activity/index.php
@@ -2,9 +2,10 @@
 /**
  * BuddyPress Activity templates
  *
- * @since      2.3.0
- * @package    BuddyPress
- * @subpackage BuddyPressActivity
+ * @since 2.3.0
+ *
+ * @package BuddyPress
+ * @subpackage bp-legacy
  */
 
 /**

src/bp-templates/bp-legacy/buddypress/assets/_attachments/avatars/camera.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * BuddyPress Avatars camera template
+ * BuddyPress Avatars camera template.
  *
- * This template is used to create the camera Backbone views
+ * This template is used to create the camera Backbone views.
  *
  * @since 2.3.0
  *

src/bp-templates/bp-legacy/buddypress/assets/_attachments/avatars/crop.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * BuddyPress Avatars crop template
+ * BuddyPress Avatars crop template.
  *
- * This template is used to create the crop Backbone views
+ * This template is used to create the crop Backbone views.
  *
  * @since 2.3.0
  *

src/bp-templates/bp-legacy/buddypress/assets/_attachments/avatars/index.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * BuddyPress Avatars main template
+ * BuddyPress Avatars main template.
  *
  * This template is used to inject the BuddyPress Backbone views
  * dealing with avatars.
- * It's also used to create the common Backbone views
+ *
+ * It's also used to create the common Backbone views.
  *
  * @since 2.3.0
  *

src/bp-templates/bp-legacy/buddypress/assets/_attachments/cover-images/index.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * BuddyPress Cover Images main template
+ * BuddyPress Cover Images main template.
  *
  * This template is used to inject the BuddyPress Backbone views
  * dealing with cover images.
- * It's also used to create the common Backbone views
+ *
+ * It's also used to create the common Backbone views.
  *
  * @since 2.4.0
  *
  * @package BuddyPress
  * @subpackage bp-attachments
  */
+
 ?>
 
 <div class="bp-cover-image"></div>

src/bp-templates/bp-legacy/buddypress/assets/_attachments/uploader.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * BuddyPress Uploader templates
+ * BuddyPress Uploader templates.
  *
- * This template is used to create the BuddyPress Uploader Backbone views
+ * This template is used to create the BuddyPress Uploader Backbone views.
  *
  * @since 2.3.0
  *

commit 712d10a8fb04c9b89b7faf6890d427311b8733ca

Templates: fix extra PHP bracket typo
Introduced recently in r10181

git-svn-id: http://buddypress.svn.wordpress.org/trunk@10235 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-templates/bp-legacy/buddypress/members/single/settings/notifications.php
@@ -6,8 +6,6 @@
  * @subpackage bp-legacy
  */
 
-?>
-
 /** This action is documented in bp-templates/bp-legacy/buddypress/members/single/settings/profile.php */
 do_action( 'bp_before_member_settings_template' ); ?>
 